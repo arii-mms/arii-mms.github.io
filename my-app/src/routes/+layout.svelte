30390ec1bce81f4afde7868dd0568080f8b9d0a1
arii-mms <129689424+arii-mms@users.noreply.github.com>
2026-06-11 18:44:48 -0300

    svelte teste

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './Header.svelte';
 	import './layout.css';
+	export const prerender = true;
 
 	let { children } = $props();
 </script>
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,49 +1,23 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
-import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
 
-export default defineConfig({
-	plugins: [
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
+const dev = process.env.NODE_ENV === 'development';
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
-		})
-	],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.js',
-				test: {
-					name: 'client',
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
-				}
-			},
-
-			{
-				extends: './vite.config.js',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
-});
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+  kit: {
+    adapter: adapter({ pages: 'build', assets: 'build', fallback: null }),
+    paths: { base: dev ? '' : '' }
+  },
+  preprocess: [mdsvex()],
+  extensions: ['.svelte', '.svx'],
+  plugins: [
+    sveltekit({
+      preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
+      extensions: ['.svelte', '.svx', '.md']
+    })
+  ]
+};
